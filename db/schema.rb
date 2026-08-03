690396ce0d901d4e6bd18a3450b9e82821ab10f6
Johnathan Lyman <jelyman@gmail.com>
2026-08-03 09:36:45 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_03_160000) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_03_170000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_03_160000) do
   create_table "monitoring_locations", force: :cascade do |t|
     t.boolean "active", default: true, null: false
     t.string "agency_code", default: "USGS", null: false
+    t.string "agency_name"
     t.string "county_code"
     t.string "county_name"
     t.datetime "created_at", null: false
